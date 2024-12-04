b4b361aa541c1358179d754e85c81133faa0345b
7erhiy <7serhiy@gmail.com>
2024-12-04 16:51:45 -0400

    pip

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,14 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
-import dynamic from "next/dynamic";
-
-const Hero = dynamic(() => import("@/components/hero"), { ssr: false });
-const Diagram = dynamic(() => import("@/components/diagram"), { ssr: false });
-const Conversatios = dynamic(() => import("@/components/conversatios"), {
-  ssr: false,
-});
+import React from "react";
+import Diagram from "@/components/diagram";
+import Hero from "@/components/hero";
+import Conversatios from "@/components/conversatios";
 
 export default function Home() {
-  const [isDiagramLoaded, setIsDiagramLoaded] = useState(false);
-  const [isConversatiosLoaded, setIsConversatiosLoaded] = useState(false);
-
-  useEffect(() => {
-    const loadDiagram = async () => {
-      await import("@/components/diagram");
-      setIsDiagramLoaded(true);
-    };
-
-    const loadConversatios = async () => {
-      await import("@/components/conversatios");
-      setIsConversatiosLoaded(true);
-    };
-
-    loadDiagram().then(loadConversatios);
-  }, []);
-
   return (
     <div className="flex flex-col overflow-hidden">
       <Hero />
-      {isDiagramLoaded && <Diagram />}
-      {isConversatiosLoaded && <Conversatios />}
+      <Diagram />
+      <Conversatios />
     </div>
   );
 }
